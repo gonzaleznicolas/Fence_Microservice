09f042ed5c06762c4225d733c2017e7b7170e589
gonzaleznicolas <nicolas.g96@gmail.com>
2020-04-01 08:52:15 -0600

    first commit

---
+++ routes.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	let someObject = {title: "hi", id: 4};
+  res.json(someObject);
+});
+
+module.exports = router;
