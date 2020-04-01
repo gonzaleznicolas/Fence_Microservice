7e7fe81438c253e2e189d5fd006551fe4cf59ab3
gonzaleznicolas <nicolas.g96@gmail.com>
2020-04-01 08:59:11 -0600

    removed unecessary file

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ var indexRouter = require('./routes');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
